f63b91932927acb3c02fa5d0c8881a2c7cc420d1
Alex Walchli <alexwalchli@gmail.com>
2016-12-06 07:06:01 -0600

    removed intellisense

--- src/js/intellisense/helpers/intellisense-helpers.js
+++
@@ -1,8 +0,0 @@
-
-export const autoCompleteQueryWithSelectedSuggestion = (caretPosition, inputValue, query, suggestion) => {
-  const suggestionText = suggestion.trigger + suggestion.label
-  const beginningPlusSuggestion = inputValue.substr(0, caretPosition.selectionStart - query.length) + suggestionText
-  const end = inputValue.substr(caretPosition.selectionStart)
-
-  return beginningPlusSuggestion + end
-}
